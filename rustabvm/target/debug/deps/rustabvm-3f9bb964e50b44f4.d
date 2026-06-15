20d6ed9456b4386b55377a102ec001842b505af5
MattAb <matt0abb@gmail.com>
2026-06-15 23:44:49 +0300

    untrack target folder

--- rustabvm/target/debug/deps/rustabvm-3f9bb964e50b44f4.d
+++
@@ -1,5 +0,0 @@
-/home/mattab/Desktop/dev/RustAb/rustabvm/target/debug/deps/rustabvm-3f9bb964e50b44f4.d: src/main.rs
-
-/home/mattab/Desktop/dev/RustAb/rustabvm/target/debug/deps/rustabvm-3f9bb964e50b44f4: src/main.rs
-
-src/main.rs:
